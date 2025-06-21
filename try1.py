3d2a0eb319b92f85187575f22ffa6806917ecf24
willie thomas <sthi1284@gmail.com>
2025-06-21 13:02:01 -0400

    try except else , error handle

---
+++ try1.py
@@ -0,0 +1,15 @@
+try :
+    number = int(input("Enter a number : "))
+    result = 10 / number
+except ValueError : 
+    print("invaild input! Please enter a vaild number ")
+
+else:    
+
+
+    print(f"The result is : {result}")
+    print("This program ends here ")
+    print("happy coding  ")
+
+
+    
